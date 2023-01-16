9901320d6a1dfdde861285418a603ac43e20634c
Czicken <avdeeva13.katerinaa@gmail.com>
2023-01-16 21:27:01 +0100

    automated age
    Co-authored-by: valivia <valivia@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,14 @@
+function dateToAge(date) {
+  const dob = new Date(date);
+  const diff_ms = Date.now() - dob.getTime();
+  const age_dt = new Date(diff_ms);
+  const age = Math.abs(age_dt.getUTCFullYear() - 1970);
+
+  return age;
+}
+
+window.addEventListener('load', () => {
+  const ageInYears = dateToAge("2005-06-17");
+  const age = document.getElementById("age");
+  age.innerHTML = ageInYears
+});
